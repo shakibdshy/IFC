e20f7e126b0cd2cf96ff70511a59fdfd87da72b7
shakibdshy <shakibdshy@gmail.com>
2018-11-05 18:39:12 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+	//Featured On Slide
+	$('.slider-active').owlCarousel({
+		autoplay: true,
+	    loop:true,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1,
+	        },
+	        1000:{
+	            items:1,
+	        }
+	    }
+	});
+
+
+});
